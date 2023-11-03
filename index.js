933756403779fadfec7af1f0347d2ae30155d8fc
Jefferson Cezar <jeffersoncezar@tokenlab.com.br>
2023-11-03 10:35:33 -0300

    verificação de acesso

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get("/pay", async (req, res) => {
             }
         ],
         payer: {
-            email: emailPay
+            email: emailPay 
         },
         external_reference: _id,
     } 
